0e9e5f2a74ac8738bc1c28ce4ed25852007a1ce3
SadeghHayeri <hayerisadegh@gmail.com>
2017-09-09 19:06:40 +0430

    solved!

--- questions/003 bomb_baby/solution.py
+++ questions/003 bomb_baby/solution.py
@@ -1,2 +1,18 @@
-def​ ​answer(M,​ ​F):
-​ ​​ ​​ ​​ ​#​ ​your​ ​code​ ​here
+def answer(M, F):
+    M, F = int(M), int(F)
+    big, small = (M, F) if M > F else (F, M)
+    
+    genaration_count = 0
+    
+    while small >= 1:
+        if big == small:
+            break
+        
+        h = ((big-1)/small)
+        genaration_count += h
+        big -= small * h
+
+        big, small = (big, small) if big > small else (small, big)
+        
+        
+    return str(genaration_count) if (small == 1 and big == 1) else "impossible"
