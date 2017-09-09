8d97519247fbab2bd780264ce109a76f87cd1ea1
SadeghHayeri <hayerisadegh@gmail.com>
2017-09-09 23:45:56 +0430

    solved!

--- questions/004 Fuel Injection Perfection/solution.py
+++ questions/004 Fuel Injection Perfection/solution.py
@@ -1,2 +1,18 @@
-def​ ​answer(n):
-​ ​​ ​​ ​​ ​#​ ​your​ ​code​ ​here
+def answer(n):
+    n = int(n)
+
+    table = {1: 0, 2: 1}
+
+    def answer_helper(n):
+        if n in table:
+            return table[n]
+
+        if n % 2 != 0:
+            table[n] = min(answer_helper((n + 1) / 2) + 2,
+                           answer_helper((n - 1) / 2) + 2)
+        else:
+            table[n] = answer_helper(n / 2) + 1
+
+        return table[n]
+
+    return answer_helper(n)
